fbf31de57bad0ac1441c79208eb3fc4252ee5716
Chelovek01 <ntl5551@mail.ru>
2022-06-22 13:00:35 +0300

    Initial commit

---
+++ bboard/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+
+from .forms import BbForms
+
+from .models import Bb, Rubric
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class =
